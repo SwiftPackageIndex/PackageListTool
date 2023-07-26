916d42df0f0e06afb20eb273870a5f17a107b233
Dave Verwer <dave.verwer@gmail.com>
2023-07-26 15:55:27 +0100

    Remove `activity`, `authors`, and `stars`.

--- Sources/PackageListTool/Models/API+APIPackage.swift
+++ Sources/PackageListTool/Models/API+APIPackage.swift
@@ -15,12 +15,8 @@
 
 extension API {
     struct APIPackage: Codable {
-        var activity: Activity?
-        var authors: AuthorMetadata
-        var history: History
         var platformCompatibility: [PlatformCompatibility]
         var license: License
-        var stars: Int
         var swiftVersionCompatibility: [SwiftVersion]
         var summary: String?
         var title: String
@@ -30,36 +26,9 @@ extension API {
 
 
 extension API.APIPackage {
-    var activityClause: String {
-        let clause = "In development for \(since: history.createdAt)"
-        if let latest = [activity?.lastIssueClosedAt, activity?.lastPullRequestClosedAt].compactMap({ $0 }).sorted().last {
-            return clause + ", with activity as late as \(relativeDate: latest)."
-        } else {
-            return clause + "."
-        }
-    }
-
-    var authorClause: String {
-        switch authors {
-            case let .fromGitRepository(authors):
-                return "Written by \(authors.authors.map(\.name).joined(separator: ", ")) and \(pluralizedCount: authors.numberOfContributors, singular: "other contributor")."
-            case let .fromSPIManifest(string):
-                return "Written by \(string)"
-        }
-    }
-}
-
-
-extension API.APIPackage {
     static var example: Self {
-        .init(activity: .init(openIssuesCount: 12, openPullRequestsCount: 12, lastIssueClosedAt: .example, lastPullRequestClosedAt: .example),
-              authors: .fromGitRepository(.init(authors: [.init(name: "Foo Bar")], numberOfContributors: 5)),
-              history: .init(createdAt: .example,
-                             commitCount: 433, commitCountURL: "https://github.com/foo/bar/commits/main",
-                             releaseCount: 5, releaseCountURL: "https://github.com/foo/bar/releases"),
-              platformCompatibility: [.macOS, .linux],
+        .init(platformCompatibility: [.macOS, .linux],
               license: .mit,
-              stars: 1234,
               swiftVersionCompatibility: [.init(major: 5, minor: 8, patch: 0), .init(major: 5, minor: 7, patch: 0)],
               summary: "Foo bar test package",
               title: "Foo",
